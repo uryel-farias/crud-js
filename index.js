9f41eeec3582ecce9517d386cdd4d6f5199982dd
Uryel Farias <uryel.farias@gmail.com>
2025-03-10 09:46:04 -0300

    Criando um elemento li e div para a lista de itens

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ botaoAdicionar.addEventListener("click",(evento) => {
     evento.preventDefault();
     
     const inputItem = document.querySelector("#input-item").value;
-    console.log(inputItem);
+
+    if (inputItem == "") {
+        alert("Por favor insira um item!")
+        return
+    } 
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    
 })
 
